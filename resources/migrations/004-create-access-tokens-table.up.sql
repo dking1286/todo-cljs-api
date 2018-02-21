575e4c21cb462464d69995f2ba4f625971812c27
Daniel King <daniel.oliver.king@gmail.com>
2018-02-20 20:23:49 -0800

    Fix incorrect access_tokens table

--- resources/migrations/004-create-access-tokens-table.up.sql
+++ resources/migrations/004-create-access-tokens-table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE access_tokens (
   id SERIAL PRIMARY KEY,
   client_id INTEGER NOT NULL REFERENCES clients (id) ON DELETE CASCADE,
-  user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE
+  user_id INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+  token VARCHAR(32) NOT NULL CHECK (character_length(token) = 32)
 );
